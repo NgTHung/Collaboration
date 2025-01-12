020e137d48f8afb0fa1593fbb9c43c823d18297c
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2025-01-12 05:36:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/json/:filename', (req, res) => {
     });
 });
 
-app.get("/test", (req, res) => {
+app.get("/hoiahgoigiho", (req, res) => {
   res.json({
     status: "✅ Offline",
     from: host,
