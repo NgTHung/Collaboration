e38153c6169ab37da7760db7430e8c07da1193c5
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2025-01-12 05:58:33 -0800

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.get("/nguyentienson", (req, res) => {
   `);
 });
 
-app.get("/notkhang", (req, res) => {
+app.get("/notnotkhang", (req, res) => {
   res.send(`
     <h1>hioegaw</h1>
     <hghiueig
